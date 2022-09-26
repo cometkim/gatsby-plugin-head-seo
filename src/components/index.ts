bb4db2a9394ff3780d120466dd5aa3df538da70d
Hyeseong Kim <hey@hyeseong.kim>
2022-09-26 18:06:18 +0900

    add components entry

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as BasicMeta } from './BasicMeta';
+export { default as OpenGraph } from './OpenGraph';
+export { default as Facebook } from './Facebook';
+export { default as TwitterCard } from './TwitterCard';
+export { default as Robots } from './Robots';
